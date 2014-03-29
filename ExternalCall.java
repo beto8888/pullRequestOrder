55e4a3dc99c69a5a6aa925d478dec7e382639fcd
Thomas Brenner <to.bre@gm.de>
2014-03-29 13:33:26 +0100

    first commit

---
+++ ExternalCall.java
@@ -0,0 +1,8 @@
+public class ExternalCall {
+
+	public static void main(final String[] args) {
+		Main mainClass = new Main();
+		System.out.println("ExternalCall");
+	}
+	
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(final String[] args) {
+		Printer.print("Hello World!");
+	}
+	
+	public static void callAFunction() {
+		System.out.println("Function Called");
+	}
+
+}
